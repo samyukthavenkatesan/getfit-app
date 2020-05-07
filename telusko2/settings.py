7a7a8ebb72a6378ab6675c80ace7bbf92de69a15
samyuktha <samyukthav10@gmail.com>
2020-05-07 16:10:47 +0530

    kio

--- telusko2/settings.py
+++ telusko2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ayh&#htk==(k-s9pbz1k^(-27qk(63o2^#6jxi-_&@&#k&j!ms'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
